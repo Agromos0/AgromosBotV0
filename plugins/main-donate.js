aa8c1bcb2b7f27ccb7b3901d5a28bdf2de5cc7fb
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2023-06-23 17:22:56 -0600

    Update main-donate.js

--- plugins/main-donate.js
+++ plugins/main-donate.js
@@ -4,13 +4,14 @@ let handler = async(m, { conn, usedPrefix, command }) => {
     let don = `
 ≡ *DONACION*
 puedes donar si quieres ayudar a mantener el bot activo
+y recibes 500k de puntos que puedes cambiar por dinero real
 
 ▢ *PayPal*
-• *Link :* https://paypal.me/fg98f
+• *correo :* soyrwy242@gmail.com
 
-▢ *Mercado Pago Arg*
-• *Alias :* fg98ff
-• *N° :* 1168352204
+▢ *tranferencia banco azteca*
+• *Alias :* angel pantoja
+• *clave inter :* 1271 8001 6091 1755 79
 `
 let img = 'https://i.ibb.co/37FP2bk/donate.jpg'
 conn.sendFile(m.chat, img, 'img.jpg', don, m)
