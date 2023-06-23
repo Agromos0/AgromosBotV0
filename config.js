31958e22b08aafc0e9eefd74c8dbc049a4dddfcb
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2023-06-23 17:39:35 -0600

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['59172945992', 'FG98', true],
-  ['5492622271736'], 
-  ['5492622271736'] 
+  ['523931806576', 'AngelSp', true],
+  ['523326380972'], 
+  ['523326380972'] 
 ] //Numeros de owner 
 
-global.mods = ['573125484672'] 
-global.prems = ['50489079501', '5219631769130', '573125484672']
+global.mods = ['523931806576'] 
+global.prems = ['523931806576', '5219631769130', '573125484672']
 global.APIs = { // API Prefix
   // name: 'https://website'
   xteam: 'https://api.xteam.xyz', 
@@ -25,14 +25,14 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'DyLux┃ᴮᴼᵀ' 
-global.author = '@fg98' 
-global.fgig = '▢ Sígueme en Instagram\nhttps://www.instagram.com/fg98_ff\n' 
-global.dygp = 'https://chat.whatsapp.com/IO9jmpI72ejHiN4unRZleU'
-global.fgsc = 'https://github.com/FG98F/dylux-fg' 
-global.fgyt = 'https://youtube.com/fg98f'
-global.fgpyp = 'https://paypal.me/fg98f'
-global.fglog = 'https://i.imgur.com/Owmb93c.png' 
+global.packname = 'Toxi-Bot' 
+global.author = '@AngelSp' 
+global.fgig = 'https://www.paypal.me/ToxicBotw' 
+global.dygp = 'https://chat.whatsapp.com/Do0jEl7ss4e8wZ2Q0cOq3u'
+global.fgsc = 'https://www.paypal.me/ToxicBotw' 
+global.fgyt = 'https://youtube.com/Toxic'
+global.fgpyp = 'https://www.paypal.me/ToxicBotw'
+global.fglog = 'https://www.paypal.me/ToxicBotw' 
 
 global.wait = '*⌛ _Cargando..._*\n*▰▰▰▱▱▱▱▱*'
 global.rwait = '⌛'
